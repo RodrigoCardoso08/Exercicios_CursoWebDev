d9eb14e47fcd879bc2767623055b0f77b3791d51
RodrigoCardoso08 <rodrigocardosoo15@gmail.com>
2022-02-24 17:34:40 -0300

    Exercicio3_Feito

---
+++ Exercicio2.js
@@ -0,0 +1,4 @@
+function idade_em_dias (anos){
+    return anos*365
+}
+console.log(idade_em_dias(25))
---
+++ Exercicio3.js
@@ -0,0 +1,4 @@
+function salario (horas_trabalhadas, valor_por_hora){
+    return "Salario igual a R$" + horas_trabalhadas*valor_por_hora 
+}
+console.log (salario(10,10))
