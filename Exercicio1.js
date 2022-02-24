7836d42e78751821ebf41fbdc0fb4cc7fd416860
RodrigoCardoso08 <rodrigocardosoo15@gmail.com>
2022-02-24 17:25:44 -0300

    Exercicio1_pronto(duas soluções)

--- Exercicio1.js
+++ Exercicio1.js
@@ -2,3 +2,11 @@ const exemplo = function concatena (text){
     return "Olá, " + text + "!"
 }
 console.log(exemplo("rodrigo")) //Quando eu for passar um argumento que é uma string tenho que colocar ""
+
+const exemplo2 = function concatena2 (text){
+    const saudacao = "Olá"
+    return [saudacao, text].join(', ').concat("!")
+}
+
+//Exemplo1 e Exemplo2 dão a mesma saida.
+console.log(exemplo2("rodrigo"))
