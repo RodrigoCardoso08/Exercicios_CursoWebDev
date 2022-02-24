7553c5686d9c24ee9d8db548c3050db624af4f05
RodrigoCardoso08 <rodrigocardosoo15@gmail.com>
2022-02-24 17:17:06 -0300

    Exercicio1_feito

---
+++ Exercicio1.js
@@ -0,0 +1,4 @@
+const exemplo = function concatena (text){
+    return "Olá, " + text + "!"
+}
+console.log(exemplo("rodrigo")) //Quando eu for passar um argumento que é uma string tenho que colocar ""
